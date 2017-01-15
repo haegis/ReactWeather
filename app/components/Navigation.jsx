b341529db76e655c9f7c987b49fd60d7b7829c0d
Werner Emmanuel Guandique <werner@haegis.io>
2017-01-15 15:14:24 -0500

    Added style to navigation bar

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -1,15 +1,46 @@
 var React = require("react");
 var {Link, IndexLink} = require("react-router");
 
-var Navigation = (props) => {
-  return (
-    <div>
-      <h1>Nav</h1>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: "bold"}}>Get weather</IndexLink>
-      <Link to="/About" activeClassName="active" activeStyle={{fontWeight: "bold"}}>About</Link>
-      <Link to="/Examples" activeClassName="active" activeStyle={{fontWeight: "bold"}}>Examples</Link>
-    </div>
-  );
-}
+var Navigation = React.createClass({
+  render: function() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: "bold"}}>Get weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/About" activeClassName="active" activeStyle={{fontWeight: "bold"}}>About</Link>
+            </li>
+            <li>
+              <Link to="/Examples" activeClassName="active" activeStyle={{fontWeight: "bold"}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+            <form onSubmit={this.onSearch}>
+              <ul className="menu">
+                <li>
+                  <input type="search" placeholder="Location" />
+                </li>
+                <li>
+                  <input type="submit" className="button" value="Get weather" />
+                </li>
+              </ul>
+            </form>
+        </div>
+      </div>
+    );
+  },
+
+  onSearch : function(event) {
+    event.preventDefault();
+    alert("Not Implemented.");
+  }
+
+
+});
 
 module.exports = Navigation;
